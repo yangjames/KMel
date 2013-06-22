38117de1ad2741f478274c58fbaa0ced7eabed62
kmel <kmel@kmel3.(none)>
2013-06-22 14:24:37 -0400

    fixed the thrust calcualtion

--- trajectory_vis/thrustRange.m
+++ trajectory_vis/thrustRange.m
@@ -2,20 +2,19 @@ function [minT maxT] = thrustRange(s)
 %calcualtes the minimum and maximum thrust requires durring a trajectory
 
     if ~isempty(s)
-
+        acc_mags = zeros(length(s), length(s(1).a));
         for i=1:length(s)
-
             acc_g = s(i).a + 9.8.*[zeros(1,length(s(i).a)); zeros(1,length(s(i).a)); ones(1,length(s(i).a))];
-            acc_mags=zeros(length(s), length(acc_g));
             for j = 1:length(acc_g)
                 acc_mags(i,j) = norm(acc_g(:,j));
             end
-            maxA = max(max(acc_mags));
-            maxT = maxA/9.8;
-
-            minA = min(min(acc_mags)); % we need to take into account the direction
-            minT = minA/9.8;
+            
         end
+        maxA = max(max(acc_mags));
+        maxT = maxA/9.8;
+
+        minA = min(min(acc_mags)); % we need to take into account the direction
+        minT = minA/9.8;
     end
 
 end
